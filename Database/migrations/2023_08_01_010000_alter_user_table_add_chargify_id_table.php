2f725e8398ddc30bb43fe2107defd8dfef369375
Mood Osman <m.osman@dreamod.pl>
2023-10-16 14:22:21 +0200

    adding migration to alter users add chargify_id

--- Database/migrations/2023_08_01_010000_alter_user_table_add_chargify_id_table.php
+++ Database/migrations/2023_08_01_010000_alter_user_table_add_chargify_id_table.php
@@ -22,7 +22,7 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::table('subscriptions', function (Blueprint $table) {
+        Schema::table('users', function (Blueprint $table) {
             $table->dropColumn(['chargify_id']);
         });
     }
