4de187cd3d6a82edf63cb4cb75e15998401d57e3
Mood Osman <m.osman@dreamod.pl>
2023-11-14 16:25:49 +0100

    update namespace and cleaning up

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -14,7 +14,7 @@ class TestCase extends \Orchestra\Testbench\TestCase
     protected function defineDatabaseMigrations()
     {
         $this->loadMigrationsFrom(['--path' => [
-            __DIR__.DIRECTORY_SEPARATOR.'database'.DIRECTORY_SEPARATOR.'migrations',
+            __DIR__.DIRECTORY_SEPARATOR.'Database'.DIRECTORY_SEPARATOR.'migrations',
             __DIR__.DIRECTORY_SEPARATOR.'..'.DIRECTORY_SEPARATOR.'Database'.DIRECTORY_SEPARATOR.'migrations',
         ],
         ]);
