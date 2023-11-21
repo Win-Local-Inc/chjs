23342fc221297f7bc55b79891e04035123990b17
Mood Osman <m.osman@dreamod.pl>
2023-11-21 11:13:03 +0100

    adding sharecard prices

--- src/Enums/ShareCardPricing.php
+++ src/Enums/ShareCardPricing.php
@@ -6,4 +6,7 @@ enum ShareCardPricing: string
 {
     use EnumHelpers;
     case ZERO = 'sharecard_zero';
+    case MONTH = 'sharecard_month';
+    case BIANNUAL = 'sharecard_biannual';
+    case YEAR = 'sharecard_year';
 }
