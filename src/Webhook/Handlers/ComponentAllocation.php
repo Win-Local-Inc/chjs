92dd390fa98b5b0b9f1f74e7472ce64389b2bde9
Piotrek <piotr.blaszkowski@winlocal.com>
2023-11-22 13:19:38 +0100

    update webhook component allocation

--- src/Webhook/Handlers/ComponentAllocation.php
+++ src/Webhook/Handlers/ComponentAllocation.php
@@ -15,7 +15,7 @@ class ComponentAllocation extends AbstractHandler
     protected function handleEvent(string $event, array $payload)
     {
         // need to get unit_price and handle
-        $pricePoint = maxio()->componentPrice->find($payload['price_point_id'])->price_point;
+        $pricePoint = maxio()->componentPrice->find($payload['price_point_id']);
         // all prices will be per_unit so only 1 element in array
         $newPrice = (int) $pricePoint->prices[0]->unit_price * (int) $payload['allocated_quantity'];
 
